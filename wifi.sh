5c2cc1ccd0abd07e726eb1a6598fc83a692618dd
jry <598821125@qq.com>
2024-11-28 09:23:48 +0800

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -1,6 +1,8 @@
 
 cd C:\\ProgramData\\HBuilderX\\plugins\\launcher-tools\\tools\\adbs
 
+cd /Applications/HBuilderX-Alpha.app/Contents/HBuilderX/plugins/launcher-tools/tools/adbs
+
 # .\\adb tcpip 5555
 
 .\\adb connect 192.168.1.99
